f94c1b4940da270b47368f772b2d96c58a8cf1ac
Bob McAleavey <bobmcaleavey@gmail.com>
2023-02-06 08:28:41 -0500

    feat: add @bobthered/tailwindcss-palette-generator

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
+const { tailwindcssPaletteGenerator } = require('@bobthered/tailwindcss-palette-generator');
+
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {
+      colors: tailwindcssPaletteGenerator({
+        colors: ['#3a41cb', '#1c78e9', '#76778f'],
+        names: ['primary', 'secondary', 'gray']
+      })
+    }
+  },
 
-	plugins: []
+  plugins: []
 };
 
 module.exports = config;
