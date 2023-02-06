91fc9a320c005effad08cb6866c70fa045d96701
Bob McAleavey <bobmcaleavey@gmail.com>
2023-02-06 08:30:24 -0500

    feat: add vite aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $auth: resolve('./src/auth'),
+      $components: resolve('./src/components'),
+      $db: resolve('./src/db'),
+      $icons: resolve('./src/icons'),
+      $lib: resolve('./src/lib'),
+      $routes: resolve('./src/routes'),
+      $src: resolve('./src')
+    }
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 };
 
 export default config;
