457c5465f335336981de0b6309b61335be434938
Bob McAleavey <bobmcaleavey@gmail.com>
2023-02-07 13:47:35 -0500

    feat: add welcome message

---
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  // props (external)
+  export let data: PageData;
+</script>
+
+<div class="flex flex-col text-[1.5rem] lg:flex-row lg:space-x-[.5rem]">
+  <div>Welcome back</div>
+  <div class="font-semibold">{data.user.firstName} {data.user.lastName}!</div>
+</div>
