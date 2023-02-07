b7c7014a38540a9c74f35bd4b14fd8b44eb332c7
Bob McAleavey <bobmcaleavey@gmail.com>
2023-02-07 13:04:22 -0500

    feat: add PasswordInput component

---
+++ src/components/PasswordInput/PasswordInput.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { current_component } from 'svelte/internal';
+  import { Button, Icon, Input } from 'sveltewind/components';
+  import { getEvents } from 'sveltewind/actions';
+  import { Eye, EyeOff } from '$icons';
+
+  // handlers
+  const toggleType = () => {
+    type = type === 'password' ? 'text' : 'password';
+    console.log(type);
+  };
+
+  // props (internal)
+  const events = getEvents(current_component);
+
+  // props (external)
+  export let type = 'password';
+  export let use = [];
+</script>
+
+<div class="relative flex">
+  <Input {...$$restProps} class="pr-[3rem] w-full {$$props.class}" {type} use={[events, ...use]} />
+  <Button
+    class="absolute top-1/2 right-[0rem] -translate-y-1/2 px-[.5rem] bg-transparent hover:bg-black/[.1] focus:bg-black/[.1] focus:ring-black/[.3] dark:bg-transparent dark:hover:bg-white/[.1] dark:focus:bg-white/[.1] dark:focus:ring-white/[.3]"
+    on:click={toggleType}
+    tabindex="-1"
+  >
+    <Icon src={type === 'password' ? Eye : EyeOff} />
+  </Button>
+</div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from 'sveltewind/components';
+export { default as PasswordInput } from './PasswordInput/PasswordInput.svelte';
 export { default as RouteTitle } from './RouteTitle/RouteTitle.svelte';
