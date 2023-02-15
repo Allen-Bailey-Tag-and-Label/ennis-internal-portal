3e3d396e6107a927cc82524b0631ccd770459dae
Bob McAleavey <bobmcaleavey@gmail.com>
2023-02-15 13:31:51 -0500

    fix: change to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
